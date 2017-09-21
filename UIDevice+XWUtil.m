671539ad30ebf67fc6be3507976020b8a4655996
zengchao <zengc@xweisoft.com>
2017-09-21 16:55:45 +0800

    add Phone8、X
    add Phone8、X

--- UIDevice+XWUtil.m
+++ UIDevice+XWUtil.m
@@ -48,6 +48,13 @@
         if ([platform isEqualToString:@"iPhone9,3"])    return @"iPhone 7";
         if ([platform isEqualToString:@"iPhone9,4"])    return @"iPhone 7 Plus";
 
+        if ([platform isEqualToString:@"iPhone10,1"])   return @"iPhone 8";
+        if ([platform isEqualToString:@"iPhone10,4"])   return @"iPhone 8";
+        if ([platform isEqualToString:@"iPhone10,2"])   return @"iPhone 8 Plus";
+        if ([platform isEqualToString:@"iPhone10,5"])   return @"iPhone 8 Plus";
+        if ([platform isEqualToString:@"iPhone10,3"])   return @"iPhone X";
+        if ([platform isEqualToString:@"iPhone10,6"])   return @"iPhone X";
+        
         return @"iPhone";
     }
 
